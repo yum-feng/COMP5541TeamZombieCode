e5a79cf5bb5c0fb6e6605286c94f0d5ada48ffd3
chrissavov <chrissavov@hotmail.com>
2022-04-06 04:27:55 -0400

    Not ready for testing yet

--- src/main/java/teamzombie/pokez/client/EventHandlers.java
+++ src/main/java/teamzombie/pokez/client/EventHandlers.java
@@ -6,10 +6,18 @@ import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import teamzombie.pokez.PokeZ;
+import teamzombie.pokez.entities.Pikachu_Entity_Model;
 import teamzombie.pokez.setup.Registration;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, modid = PokeZ.MODID, value = Dist.CLIENT)
 public class EventHandlers {
+
+    private void ClientModEvents() {}
+
+    @SubscribeEvent
+    public static void registerLayers(EntityRenderersEvent.RegisterLayerDefinitions event) {
+        event.registerLayerDefinition(Pikachu_Entity_Model.LAYER_LOCATION, Pikachu_Entity_Model::createBodyLayer);
+    }
 
     @SubscribeEvent
     public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
